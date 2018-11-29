05dc5ac71d6eb9d38c521f4e7acc345cbbd99edc
quintinlopes <quintinlopes@gmail.com>
2018-11-29 18:40:51 -0500

    initial site files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,53 @@
-$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyCdCnaio1S5mWGdR9BlEjl8OGi1PO1TTSM",
+        authDomain: "train-scheduler-4bc3e.firebaseapp.com",
+        databaseURL: "https://train-scheduler-4bc3e.firebaseio.com",
+        projectId: "train-scheduler-4bc3e",
+        storageBucket: "train-scheduler-4bc3e.appspot.com",
+        messagingSenderId: "336775941029"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+$("#submitTrain").on('click', function(event) {
+    event.preventDefault();
+
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var trainTime = moment($("#trainTime").val().trim(), "HH:MM").format("X");
+    var frequency = $("#frequency").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        time: trainTime,
+        frequency: frequency,
+    };
+
+    database.ref().push(newTrain);
+
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#trainTime").val("");
+    $("#frequency").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var frequency = childSnapshot.val().frequency;
+
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(trainTime),
+        $("<td>").text(frequency),
+        $("<td>").text(minutesAway),
+    );
 
 });
