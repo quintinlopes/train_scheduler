1d26d176d07e9f06bc374448488aa9aa34ad3474
quintinlopes <quintinlopes@gmail.com>
2019-02-26 12:08:37 -0500

    initial site files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCdCnaio1S5mWGdR9BlEjl8OGi1PO1TTSM",
+    authDomain: "train-scheduler-4bc3e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4bc3e.firebaseio.com",
+    projectId: "train-scheduler-4bc3e",
+    storageBucket: "train-scheduler-4bc3e.appspot.com",
+    messagingSenderId: "336775941029"
+};
+firebase.initializeApp(config);
 
-    var config = {
-        apiKey: "AIzaSyCdCnaio1S5mWGdR9BlEjl8OGi1PO1TTSM",
-        authDomain: "train-scheduler-4bc3e.firebaseapp.com",
-        databaseURL: "https://train-scheduler-4bc3e.firebaseio.com",
-        projectId: "train-scheduler-4bc3e",
-        storageBucket: "train-scheduler-4bc3e.appspot.com",
-        messagingSenderId: "336775941029"
-    };
-    firebase.initializeApp(config);
-
-    var database = firebase.database();
+var database = firebase.database();
 
 $("#submitTrain").on('click', function(event) {
     event.preventDefault();
@@ -49,5 +48,6 @@ database.ref().on("child_added", function(childSnapshot) {
         $("<td>").text(frequency),
         $("<td>").text(minutesAway),
     );
-
+    
+    $("#addTrain").append(newTrain());
 });
