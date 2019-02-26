468147ee108faa679b7d9cbe979ef6e0e93e84e0
quintinlopes <quintinlopes@gmail.com>
2019-02-26 17:04:35 -0500

    intital site files

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,12 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#submitTrain").on('click', function(event) {
+$("#submitTrain").on("click", function(event) {
     event.preventDefault();
 
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var trainTime = moment($("#trainTime").val().trim(), "HH:MM").format("X");
+    var trainTime = $("#trainTime").val().trim();
     var frequency = $("#frequency").val().trim();
 
     var newTrain = {
@@ -33,21 +33,43 @@ $("#submitTrain").on('click', function(event) {
     $("#frequency").val("");
 });
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function(snapshot) {
 
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var trainTime = childSnapshot.val().time;
-    var frequency = childSnapshot.val().frequency;
+    var trName = snapshot.val().name;
+    var weGoing = snapshot.val().destination;
+    var firstTT = snapshot.val().time;
+    var trFrequency = snapshot.val().frequency;
 
+    var fttConverted = moment(firstTT, "hh:mm").subtract(1, "years");
 
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destination),
-        $("<td>").text(trainTime),
-        $("<td>").text(frequency),
-        $("<td>").text(minutesAway),
-    );
+    var curTime = moment();
+
+    var diffTime = moment().diff(moment(fttConverted), "minutes");
+
+    var tRemainder = diffTime % trFrequency;
+
+    var trMinUntilTrain = trFrequency - tRemainder;
+
+    var nextTrain = moment().add(trMinUntilTrain, "minutes");
+
+    var newRow = $("<tr>");
+
+        var col1 = $("<td>" + snapshot.val().name + "</td>");
+        var col2 = $("<td>" + snapshot.val().destination + "</td>");
+        var col3 = $("<td>" + snapshot.val().time + "</td>");
+        var col4 = $("<td>" + moment(nextTrain).format("hh:mm") + "</td>");
+        var col5 = $("<td>" + trMinUntilTrain + "</td>");
+
+        $(col1).appendTo(row);
+        $(col2).appendTo(row);
+        $(col3).appendTo(row);
+        $(col4).appendTo(row);
+        $(col5).appendTo(row);
+        // $("<td>").text(trainName),
+        // $("<td>").text(destination),
+        // $("<td>").text(trainTime),
+        // $("<td>").text(frequency),
+        // $("<td>").text(minutesAway),
     
-    $("#addTrain").append(newTrain());
+    $("#addTrain").append(row);
 });
