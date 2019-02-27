43356027f5f8b6133be75582c6809af36075b69a
quintinlopes <quintinlopes@gmail.com>
2019-02-27 16:53:20 -0500

    plz work

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ $("#submitTrain").on("click", function(event) {
         name: trainName,
         destination: destination,
         time: trainTime,
-        frequency: frequency,
+        frequency: frequency
     };
 
     database.ref().push(newTrain);
@@ -35,36 +35,42 @@ $("#submitTrain").on("click", function(event) {
 
 database.ref().on("child_added", function(snapshot) {
 
-    var trName = snapshot.val().name;
-    var weGoing = snapshot.val().destination;
+    // var trName = snapshot.val().name;
+    // var weGoing = snapshot.val().destination;
     var firstTT = snapshot.val().time;
     var trFrequency = snapshot.val().frequency;
 
     var fttConverted = moment(firstTT, "hh:mm").subtract(1, "years");
+    console.log(fttConverted);
 
     var curTime = moment();
+    console.log("CURRENT TIME: " + moment(curTime).format("hh:mm"));
 
     var diffTime = moment().diff(moment(fttConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     var tRemainder = diffTime % trFrequency;
+    console.log(tRemainder);
 
     var trMinUntilTrain = trFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + trMinUntilTrain);
 
     var nextTrain = moment().add(trMinUntilTrain, "minutes");
+    console.log("ARRIVAL: " + moment(nextTrain).format("hh:mm"));
 
     var newRow = $("<tr>");
 
         var col1 = $("<td>" + snapshot.val().name + "</td>");
         var col2 = $("<td>" + snapshot.val().destination + "</td>");
-        var col3 = $("<td>" + snapshot.val().time + "</td>");
+        var col3 = $("<td>" + snapshot.val().frequency + "</td>");
         var col4 = $("<td>" + moment(nextTrain).format("hh:mm") + "</td>");
         var col5 = $("<td>" + trMinUntilTrain + "</td>");
 
-        $(col1).appendTo(row);
-        $(col2).appendTo(row);
-        $(col3).appendTo(row);
-        $(col4).appendTo(row);
-        $(col5).appendTo(row);
+        $(col1).appendTo(newRow);
+        $(col2).appendTo(newRow);
+        $(col3).appendTo(newRow);
+        $(col4).appendTo(newRow);
+        $(col5).appendTo(newRow);
         // $("<td>").text(trainName),
         // $("<td>").text(destination),
         // $("<td>").text(trainTime),
